d7126eda69b16e64e7bb0d4043d69ba59da24308
Louis <louis.lacoste@ynov.com>
2021-02-09 09:52:37 +0100

    .

--- locasso/resources/views/main_page/search.blade.php
+++ locasso/resources/views/main_page/search.blade.php
@@ -1,6 +1,79 @@
-@extends('layouts.app') 
-@section('content')
+@extends('layouts.app') @section('content')
 
-<h1>Mes couilles</h1>
+<div class="container-fluid searchpage__formcontainer">
+  <div class="container m-auto d-flex justify-content-center">
+    <form class="w-100">
+      <div class="form-row">
+        <div class="form-group col-md-4">
+          <label for="address">Adresse</label>
+          <input type="text" class="form-control" id="address" placeholder="Où cherchez-vous ?" />
+        </div>
+        <div class="form-group col-md-4">
+          <label for="from-date">Arrivée</label>
+          <input type="date" class="form-control" id="from-date" placeholder="De quand ?" />
+        </div>
+        <div class="form-group col-md-4">
+          <label for="to-date">Départ</label>
+          <input type="date" class="form-control" id="to-date" placeholder="A quand ?" />
+        </div>
+      </div>
+      <input type="submit" value="Rechercher" class="btn btn-primary btn-lg w-100">
+    </form>
+  </div>
+</div>
+
+<div class="container-fluid searchpage__searchresults">
+  <div class="container m-auto">
+    <div class="row">
+      <div class="col-md-6">
+        <div id="searchresult-carousel" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#searchresult-carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#searchresult-carousel" data-slide-to="1"></li>
+            <li data-target="#searchresult-carousel" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="https://via.placeholder.com/450x400" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="https://via.placeholder.com/450x400" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="https://via.placeholder.com/450x400" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#searchresult-carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#searchresult-carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+      <div class="col-md-6 d-flex">
+        <div class="d-flex flex-column flex-1">
+          <h3 class="h3 mt-4">Nom</h3>
+          <small class="text-muted">120m² - Cuisine - Ménage inclus</small>
+          <div class="flex-grow-1 d-flex align-items-center">
+            <p class="text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis,lectus
+              magna fringilla urna, porttitor rhoncus dolor purus non enim
+            </p>
+          </div>
+          <div class="d-flex mb-4">
+            <div class="flex-grow-1">
+              <span class="h5">650€</span>
+              <span class="h4">Note</span>
+            </div>
+            <button class="btn btn-primary">Voir la fiche</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
--- locasso/resources/views/nav.blade.php
+++ locasso/resources/views/nav.blade.php
@@ -1,54 +1,115 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	<div class="pl-3"><img src="{{ asset('images/Logo.svg')}}"></div>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" id="menu-main">
+    <div class="pl-3"><img id="img-brand" src="{{ asset('images/Logo.svg') }}" /></div>
+    <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor01"
+        aria-controls="navbarColor01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+    >
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-	<div class="collapse navbar-collapse" id="navbarColor01">
-		<ul class="navbar-nav ml-auto">
-			<li class="nav-item {{ Route::current()->getName() == 'accueil.index' ? 'active' : '' }}">
-				<a class="nav-link"  href="{{ route('accueil.index') }}">{{ __('Accueil') }}</a>
-			</li>
-			<li class="nav-item {{ Route::current()->getName() == 'search.index' ? 'active' : '' }}">
-				<a class="nav-link"  href="{{ route('search.index') }}">{{ __('Recherche') }}</a>
-			</li>
-			<li class="nav-item {{ Route::current()->getName() == 'apropo.index' ? 'active' : '' }}">
-				<a class="nav-link" href="{{ route('apropo.index') }}">{{ __('A propos') }}</a>
-			</li>
-		@guest
-			<li class="nav-item {{ Route::current()->getName() == 'register' ? 'active' : '' }}">
-				<button class="btn-primary"><a class="nav-link text-white" href="{{ route('register') }}">{{ __('Inscription') }}</a></button>
-			</li>
-			<li class="nav-item {{ Route::current()->getName() == 'login.index' ? 'active' : '' }}">
-				 <a class="nav-link" href="{{ route('login') }}">{{ __('Connexion') }}</a>
-			</li>
-		@else
-			<li class="nav-item {{ Route::current()->getName() == 'reversation.index' ? 'active' : '' }}">
-				<a class="nav-link" href="{{ route('reversation.index') }}">{{ __('Mes réservations') }}</a>
-			</li>
-			<li class="nav-item {{ Route::current()->getName() == 'etablissement.index' ? 'active' : '' }}">
-				<a class="nav-link" href="{{ route('etablissement.index') }}">{{ __('Mon établissement') }}</a>
-			</li>
-			<li class="nav-item {{ Route::current()->getName() == 'profil.index' ? 'active' : '' }}">
-				<a class="nav-link" href="{{ route('profil.index') }}">{{ __('Mon profil') }}</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-					{{ Auth::user()->name }} <span class="caret"></span>
-				</a>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+        <ul id="navbar-menu" class="navbar-nav ml-auto">
+            <li
+                class="nav-item {{ Route::current()->getName() == 'accueil.index' ? 'active' : '' }}"
+            >
+                <a class="nav-link" href="{{ route('accueil.index') }}">{{
+                    __("Accueil")
+                }}</a>
+            </li>
+            <li
+                class="nav-item {{ Route::current()->getName() == 'search.index' ? 'active' : '' }}"
+            >
+                <a class="nav-link" href="{{ route('search.index') }}">{{
+                    __("Recherche")
+                }}</a>
+            </li>
+            <li
+                class="nav-item {{ Route::current()->getName() == 'apropo.index' ? 'active' : '' }}"
+            >
+                <a class="nav-link" href="{{ route('apropo.index') }}">{{
+                    __("A propos")
+                }}</a>
+            </li>
+            @guest
+            <li
+                class="nav-item {{ Route::current()->getName() == 'register' ? 'active' : '' }}"
+            >
+                <a class="btn btn-primary" href="{{ route('register') }}">
+                    {{ __("Inscription") }}</a
+                    >
+                </a>
+            </li>
+            <li
+                class="nav-item {{ Route::current()->getName() == 'login.index' ? 'active' : '' }}"
+            >
+                <a class="btn btn-light" href="{{ route('login') }}">{{
+                    __("Connexion")
+                }}</a>
+            </li>
+            @else
+            <li
+                class="nav-item {{ Route::current()->getName() == 'reversation.index' ? 'active' : '' }}"
+            >
+                <a class="nav-link" href="{{ route('reversation.index') }}">{{
+                    __("Mes réservations")
+                }}</a>
+            </li>
+            <li
+                class="nav-item {{ Route::current()->getName() == 'etablissement.index' ? 'active' : '' }}"
+            >
+                <a class="nav-link" href="{{ route('etablissement.index') }}">{{
+                    __("Mon établissement")
+                }}</a>
+            </li>
+            <li
+                class="nav-item {{ Route::current()->getName() == 'profil.index' ? 'active' : '' }}"
+            >
+                <a class="nav-link" href="{{ route('profil.index') }}">{{
+                    __("Mon profil")
+                }}</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a
+                    id="navbarDropdown"
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                    v-pre
+                >
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
 
-				<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-					<a class="dropdown-item" href="{{ route('logout') }}"
-						onclick="event.preventDefault();
-										document.getElementById('logout-form').submit();">
-						{{ __('Deconnexion') }}
-					</a>
-					<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-						@csrf
-					</form>	
-				</div>
-			</li>
-		</ul>
-		@endguest
-	</div>
+                <div
+                    class="dropdown-menu dropdown-menu-right"
+                    aria-labelledby="navbarDropdown"
+                >
+                    <a
+                        class="dropdown-item"
+                        href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+										document.getElementById('logout-form').submit();"
+                    >
+                        {{ __("Deconnexion") }}
+                    </a>
+                    <form
+                        id="logout-form"
+                        action="{{ route('logout') }}"
+                        method="POST"
+                        style="display: none"
+                    >
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </ul>
+        @endguest
+    </div>
 </nav>
