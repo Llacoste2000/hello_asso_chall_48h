9f1f9c157a9ad93b9d6528b92e9e0bfe45909835
KuroiTenshi1 <chiquet.daniel@gmail.com>
2021-02-09 03:12:09 +0100

    .

--- locasso/resources/views/main_page/accueil.blade.php
+++ locasso/resources/views/main_page/accueil.blade.php
@@ -1,6 +1,11 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 @section('content')
+<div>
+    <div>
+        <h1>Vos critères en location</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id integer.
+    </div>
 
-<h1>Mes couilles</h1>
+</div>
 
 @endsection
